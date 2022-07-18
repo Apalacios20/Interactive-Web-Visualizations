02f3022932bcf7f8044183f70bf4f7e57703738f
Apalacios <Apalacios0020@gmail.com>
2022-07-18 16:33:26 -0700

    update

--- static/app.js
+++ static/app.js
@@ -5,3 +5,33 @@ d3.json(url).then(function(data) {
   console.log(data);
 });
 
+
+// Sample Data
+let s_data = d3.json(url).then(function(data) {
+    console.log("Samples :", data.samples);
+});
+
+
+//let values = []
+//let otu_id = []
+//let otu_label = []
+
+//for (let i = 0; i < sample.length; i++) {
+//    row = sample[i];
+//    values.push(row.sample_values) {
+//        console.log("values:", values)
+//    };
+//    otu_id.push(row.otu_ids);
+//    otu_label.push(row.otu_label);
+//    console.log("Values:", values);
+
+//console.log("Values:", values);
+// Values
+
+
+// OTU Ids
+
+
+// OTU Labels
+
+
