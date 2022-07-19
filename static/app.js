53beaa958d8822c8dce2e0b7c8899096d59ed51f
Apalacios <Apalacios0020@gmail.com>
2022-07-19 14:30:40 -0700

    update

--- static/app.js
+++ static/app.js
@@ -5,33 +5,34 @@ d3.json(url).then(function(data) {
   console.log(data);
 });
 
-
 // Sample Data
 let s_data = d3.json(url).then(function(data) {
     console.log("Samples :", data.samples);
 });
 
+// Bar chart
+let values = []
+let otu_id = []
+let otu_label = []
 
-//let values = []
-//let otu_id = []
-//let otu_label = []
+d3.json(url).then(function(data) {
+    s_data = data.samples;
+    for (let i = 0; i < s_data.length; i++) {
+        row = s_data[i];
+        values.push(row.sample_values) 
+        otu_id.push(row.otu_ids);
+        otu_label.push(row.otu_label);
+    };
+});
+console.log("value:", values[0]);
+console.log("otu id:", otu_id);
+console.log("otu label:", otu_label);
 
-//for (let i = 0; i < sample.length; i++) {
-//    row = sample[i];
-//    values.push(row.sample_values) {
-//        console.log("values:", values)
-//    };
-//    otu_id.push(row.otu_ids);
-//    otu_label.push(row.otu_label);
-//    console.log("Values:", values);
-
-//console.log("Values:", values);
-// Values
-
-
-// OTU Ids
-
-
-// OTU Labels
-
-
+var data = [{
+    type: 'bar',
+    x: values[0],
+    y: otu_id[0],
+    orientation: 'h'
+  }];
+  
+  Plotly.newPlot("bar", data);
